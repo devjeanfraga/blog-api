bd23e1c045b5481a38617e2387cadbe4611c1a22
Jean Fraga <jeandevjs@gmail.com>
2022-03-23 17:22:51 -0300

    feat: ensure SignUpController return 400 if field name missing

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Controller } from "@src/presentation/protocols/controller"
 import { HttpRequest, HttpResponse } from "@src/presentation/protocols/http"
 import { EmailValidator } from "@src/presentation/protocols/email-validator"
+import { MissingParamError } from "@src/presentation/errors/missing-param-error"
 
 export class SignUpController implements Controller{
 
@@ -12,11 +13,11 @@ export class SignUpController implements Controller{
 
   public  handle (httpRequest: HttpRequest): HttpResponse {
 
-      const fields = ['name', 'email', 'password', 'passwordConfirm']
+      const fields = [ 'name', 'email', 'password', 'passwordConfirm']
 
       for( const field of fields) {
         if(!httpRequest.body[field]) {
-          const requiredField = (): HttpResponse => ({ statusCode: 400, body: new Error('some field is required') })
+          const requiredField = (): HttpResponse => ({ statusCode: 400, body: new MissingParamError(field) })
           return requiredField()
           //return new Promise(resolve => resolve(requiredField()))
         }
