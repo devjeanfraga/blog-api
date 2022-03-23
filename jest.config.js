d727809b934bebda2a427f60070da4293ba822eb
Jean Fraga <jeandevjs@gmail.com>
2022-03-23 10:02:27 -0300

    chore: change config jest

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
 /* eslint-disable */
 
-import { resolve } from 'path'
+const { resolve } = require('path')
 
 const root = resolve(__dirname)
 
 module.exports = {
   rootDir: root,
   displayName: 'root-test',
-  testMach: ['<rootDir>/src/**/.test.ts'],
+  testMatch: ['<rootDir>/src/**/**.test.ts'],
   testEnvironment: 'node',
   clearMocks: true,
   preset: 'ts-jest',
   moduleNameMapper: {
     '@src/(.*)': '<rootDir>/src/$1'
   },
-  displayName: 'functional-test',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  // displayName: 'functional-test',
+  // testMatch: ['<rootDir>/src/**/*.spec.ts'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
