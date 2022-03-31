21f89c1ae049d7d9627c08c775fde215d0f62488
Jean Fraga <jeandevjs@gmail.com>
2022-03-31 11:32:02 -0300

    test: ensure SignupController returns 400 if EmailValidator return false

--- src/presentation/controllers/signup/signup.test.ts
+++ src/presentation/controllers/signup/signup.test.ts
@@ -2,6 +2,7 @@ import { SignUpController } from "./signup"
 import { HttpRequest } from "@src/presentation/protocols/http"
 import { EmailValidator } from "@src/presentation/protocols/email-validator"
 import { MissingParamError} from '@src/presentation/errors/missing-param-error'
+import { InvalidParamError } from "@src/presentation/errors/invalid-param-error"
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -70,9 +71,9 @@ describe('SignUp Controller', () => {
       statusCode: 400
     }
     expect(error).toEqual(response)
-})
+  })
 
-it('Should return an Error if password field missing', async () => {
+  it('Should return an Error if password field missing', async () => {
   const { sut } = makeSut()
   const fakeHttpRequest = {
     body: {
@@ -88,32 +89,44 @@ it('Should return an Error if password field missing', async () => {
     statusCode: 400
   }
   expect(error).toEqual(response)
-})
+  })
 
-it('Should return an Error if passwordConfirm field missing', async () => {
-  const { sut } = makeSut()
-  const fakeHttpRequest = {
-    body: {
-      name: 'any-name',
-      email: 'any@mail.com',
-      password: 'any-password',
-      //passwordConfirm: 'any-password'
+  it('Should return an Error if passwordConfirm field missing', async () => {
+    const { sut } = makeSut()
+    const fakeHttpRequest = {
+      body: {
+        name: 'any-name',
+        email: 'any@mail.com',
+        password: 'any-password',
+        //passwordConfirm: 'any-password'
+      }
     }
-  }
-  const error =  sut.handle(fakeHttpRequest)
-  const response = {
-    body: new MissingParamError('passwordConfirm'),
-    statusCode: 400
-  }
-  expect(error).toEqual(response)
-})
+    const error =  sut.handle(fakeHttpRequest)
+    const response = {
+      body: new MissingParamError('passwordConfirm'),
+      statusCode: 400
+    }
+    expect(error).toEqual(response)
+  })
 
   it('Should call EmailValidator with correct values',   () => {
-    const { sut, emailValidatorStub } = makeSut()
-    const spyIsValidMail = jest.spyOn(emailValidatorStub, 'isValidEmail')
+      const { sut, emailValidatorStub } = makeSut()
+      const spyIsValidMail = jest.spyOn(emailValidatorStub, 'isValidEmail')
 
-    sut.handle(makeFakeRequest())
-    expect(spyIsValidMail).toHaveBeenCalledWith(makeFakeRequest().body.email)
+      sut.handle(makeFakeRequest())
+      expect(spyIsValidMail).toHaveBeenCalledWith(makeFakeRequest().body.email)
+    })
+  
+  it('Should return an InvalidParamsErrror if Emailvalidator return false', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValidEmail').mockReturnValueOnce(false)
+
+    const promise = sut.handle(makeFakeRequest())
+    expect(promise).toEqual({
+      statusCode: 400,
+      body: new InvalidParamError('email')
+    })
+
   })
 
 
