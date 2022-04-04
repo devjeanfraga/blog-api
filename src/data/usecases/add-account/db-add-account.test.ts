72afb5ce235e4fd93a1f88318ab7c6a36651f789
Jean Fraga <jeandevjs@gmail.com>
2022-04-04 20:32:09 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.test.ts
+++ src/data/usecases/add-account/db-add-account.test.ts
@@ -1,9 +1,34 @@
 import { DbAddAccount } from './db-add-account'
 import { Hasher } from '@src/data/protocols/criptography/hasher'
-import { AddAccountModel } from '@src/domain/usecases/add-account'
+import { AddAccount, AddAccountModel } from '@src/domain/usecases/add-account'
+import { AccountModel } from '@src/domain/model/account-model'
+import { AddAccountRepository } from '@src/data/protocols/db/add-account-repository'
 
 describe('DbAddAccount', () => {
-  const makeHasherStub = (): Hasher => {
+
+  const makeFakeRequest = (): AddAccountModel => ({
+    name: 'any-name',
+    email: 'any@mail.com',
+    password: 'any-password'
+  })
+
+  const makeFakeAccount = (): AccountModel => ({
+    id: 'any-id',
+    name: 'any-name',
+    email: 'any@mail.com',
+    password: 'hashed-password'
+  })
+
+  const makeAddAccountRepository = (): AddAccountRepository => {
+    class AddAccountRepositoryStub  implements AddAccount {
+      public async add (account: AddAccountModel ): Promise<AccountModel> {   
+        return new Promise(resolve => resolve(makeFakeAccount()))
+      }
+    }
+    return new AddAccountRepositoryStub()
+  }
+
+  const makeHasher = (): Hasher => {
     class HasherStub implements Hasher {
       public hash (value: string): Promise<string> {
         return new Promise(resolver => resolver('password-hashed'))
@@ -12,22 +37,23 @@ describe('DbAddAccount', () => {
     return new HasherStub()
   }
 
-  const makeFakeRequest = (): AddAccountModel => ({
-    name: 'any-name',
-    email: 'any@mail.com',
-    password: 'any-password'
-  })
+  
+
+  
 
   interface SutTypes {
     sut: DbAddAccount
     hasherStub: Hasher
+    addAccountRepositoryStub: AddAccountRepository
   }
   const makeSut = (): SutTypes => {
-    const hasherStub = makeHasherStub()
-    const sut = new DbAddAccount(hasherStub)
+    const addAccountRepositoryStub = makeAddAccountRepository()
+    const hasherStub = makeHasher()
+    const sut = new DbAddAccount(hasherStub, addAccountRepositoryStub)
     return {
       sut,
-      hasherStub
+      hasherStub,
+      addAccountRepositoryStub
     }
   }
 
@@ -45,6 +71,18 @@ describe('DbAddAccount', () => {
     const promise = sut.add(makeFakeRequest())
     await expect(promise).rejects.toThrow()
     //Garanti que o try-catch nao será implementado justamente para o erro ser lançado a diante 
+  })
+
+  it('Should calls AddAccountRepository with corret values', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    const spyAdd = jest.spyOn(addAccountRepositoryStub, 'add')
+    
+    await sut.add(makeFakeRequest())
+    expect(spyAdd).toHaveBeenCalledWith({
+      name: 'any-name',
+      email: 'any@mail.com',
+      password: 'password-hashed'
+    })
   })
   
 })
