521bbb2d98a1072365ed4ca55f74fb5b7047e6cb
Jean Fraga <jeandevjs@gmail.com>
2022-04-20 20:47:19 -0300

    feat: add dependences in directEmail

--- src/main/factories/direct-email-factory.ts
+++ src/main/factories/direct-email-factory.ts
@@ -1,18 +1,19 @@
-import { DirectEmail } from "@src/data/usecases/direct-email";
-import { SendMail } from "@src/domain/usecases/send-mail";
-import { NodemailerAdapter } from "@src/infra/nodemailer-adapter";
+import { DirectEmail } from "@src/data/usecases/direct-email"
+import { SendMail } from "@src/domain/usecases/send-mail"
+import { JwtAdapter } from "@src/infra/cryptography/jwt-adapter"
+import { NodemailerAdapter } from "@src/infra/mail/nodemailer-adapter"
+import { GenerateUrl } from '@src/data/protocols/generate-url'
 import * as config from 'config'
-import { IConfig } from "config";
+import { IConfig } from "config"
 
 const email: IConfig = config.get('app.smtp')
+const encrypter: IConfig = config.get('app.encrypter')
 
 export const makeDirectEmail = (): SendMail => {
-  const nodeMailerAdapter = new NodemailerAdapter(
-    email.get('email'),
-    email.get('host'),
-    email.get('user'),
-    email.get('pass'),
-    parseInt(`${email.get('port')}`)
+  const jwtAdapter = new JwtAdapter(encrypter.get('secretKey'))
+  const generateUrl = new GenerateUrl()
+  const nodeMailerAdapter = new NodemailerAdapter(email.get('email'), email.get('host'), email.get('user'), email.get('pass'), parseInt(`${email.get('port')}`)
   )
-  return new DirectEmail(nodeMailerAdapter)
+  
+  return new DirectEmail(jwtAdapter, generateUrl, nodeMailerAdapter)
 }
