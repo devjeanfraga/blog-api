9f485c8bd2c53b2ed1cd22dd808938b4e1523836
Jean Fraga <jeandevjs@gmail.com>
2022-04-20 12:06:39 -0300

    feat: ensure DirectEmail calls encrypt with correct value

--- src/data/usecases/direct-email.ts
+++ src/data/usecases/direct-email.ts
@@ -1,12 +1,14 @@
 import { SendMail, SendMailParams } from "@src/domain/usecases/send-mail";
+import { Encrypter } from "../protocols/encrypter";
 import { NodemailerSendMail } from "../protocols/nodemailer-send-mail";
 
 export class DirectEmail  implements SendMail {
 
-  constructor( private readonly nodemailerSendMail: NodemailerSendMail) {}
+  constructor( private readonly encrypter: Encrypter, private readonly nodemailerSendMail: NodemailerSendMail) {}
 
   public async sendMail ( params: SendMailParams ): Promise<void> {
+    const tokenToCheckEmail = this.encrypter.encrypt(params.to)
     await this.nodemailerSendMail.sendMail( params )
-     return null
+    return null
   }
 }
