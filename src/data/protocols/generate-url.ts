23ee42a2d6ab03cd426f09bbfeb56775d0bf9598
Jean Fraga <jeandevjs@gmail.com>
2022-04-20 13:28:03 -0300

    feat: ensure DirectEmail calls generateUrl with correct values

--- src/data/protocols/generate-url.ts
+++ src/data/protocols/generate-url.ts
@@ -1,3 +1,6 @@
-export interface GenerateURL {
-  generate ( route: string, resource: string ): string
+export class GenerateUrl {
+  public generate ( route: string, token: string): string {
+    const baseURL = 'http://localhost:3838'
+    return `${baseURL}${route}${token}`
+  }
 }
--- src/data/usecases/direct-email.ts
+++ src/data/usecases/direct-email.ts
@@ -1,14 +1,22 @@
 import { SendMail, SendMailParams } from "@src/domain/usecases/send-mail";
 import { Encrypter } from "../protocols/encrypter";
+import { GenerateUrl } from "../protocols/generate-url";
 import { NodemailerSendMail } from "../protocols/nodemailer-send-mail";
 
 export class DirectEmail  implements SendMail {
 
-  constructor( private readonly encrypter: Encrypter, private readonly nodemailerSendMail: NodemailerSendMail) {}
+  constructor( 
+    private readonly encrypter: Encrypter, 
+    private readonly generateUrl: GenerateUrl, 
+    private readonly nodemailerSendMail: NodemailerSendMail
+  ) {}
 
   public async sendMail ( params: SendMailParams ): Promise<void> {
-    const tokenToCheckEmail = this.encrypter.encrypt(params.to)
-    await this.nodemailerSendMail.sendMail( params )
+    const tokenToCheckEmail = await this.encrypter.encrypt(params.to)
+    const adress =  this.generateUrl.generate('/user/check-email/', tokenToCheckEmail)
+
+    const format = { to: params.to, subject: params.subject, text: `${params.text}: ${adress}`}
+    await this.nodemailerSendMail.sendMail( format )
     return null
   }
 }
