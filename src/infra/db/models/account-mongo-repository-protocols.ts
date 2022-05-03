9bbace01d42962c481c4b930a0d0bf188efcf714
Jean Fraga <jeandevjs@gmail.com>
2022-05-03 11:25:05 -0300

    refactor: add all protocols in a single file

---
+++ src/infra/db/models/account-mongo-repository-protocols.ts
@@ -0,0 +1,4 @@
+export * from "./account-mongo-repository"
+export * from "../mongo-helper"
+export { default as AccountMongooseModel } from '../models/account-model'
+export * from "@src/domain/model/account-model"
