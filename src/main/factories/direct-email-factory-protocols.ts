1723247b40351eea8f1763a1737ecc617ef752ab
Jean Fraga <jeandevjs@gmail.com>
2022-05-03 11:24:43 -0300

    refactor: add all protocols in a single file

---
+++ src/main/factories/direct-email-factory-protocols.ts
@@ -0,0 +1,12 @@
+export * from '../../data/usecases/direct-email'
+export * from '../../data/protocols/generate-url'
+export * from '../../infra/cryptography/jwt-adapter'
+export * from '../../infra/mail/nodemailer-adapter'
+export * from '../../domain/usecases/send-mail'
+
+
+// "@src/data/usecases/direct-email"
+// '@src/data/protocols/generate-url'
+// "@src/infra/cryptography/jwt-adapter"
+// "@src/infra/mail/nodemailer-adapter"
+// "@src/domain/usecases/send-mail"
