ca37249727a9b3aa22f7a7b304e014c3fb627bb4
Jean Fraga <jeandevjs@gmail.com>
2022-05-03 11:24:20 -0300

    refactor: add all protocols in a single file

---
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -0,0 +1,14 @@
+export * from '../../../presentation/protocols'
+export * from '../../../presentation/errors'
+export * from '../../../presentation/helpers/http-helpers'
+export * from '../../../domain/usecases/add-account'
+export * from '../../../domain/usecases/send-mail'
+
+
+// import { HttpRequest, HttpResponse } from "@src/presentation/protocols/http"
+// import { EmailValidator } from "@src/presentation/protocols/email-validator"
+// import { MissingParamError } from "@src/presentation/errors/missing-param-error"
+// import { InvalidParamError } from "@src/presentation/errors/invalid-param-error"
+// import { AddAccount } from "@src/domain/usecases/add-account"
+// import { badRequest, ok, serverError } from "@src/presentation/helpers/http-helpers"
+// import { SendMail } from "@src/domain/usecases/send-mail"
