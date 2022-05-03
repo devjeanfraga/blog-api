469e8a37b68a170ab0ab5b9dd3eb18eddffe0eac
Jean Fraga <jeandevjs@gmail.com>
2022-05-03 11:24:34 -0300

    refactor: add all protocols in a single file

---
+++ src/main/factories/signup-factory-protocols.ts
@@ -0,0 +1,6 @@
+export * from '../../infra/db/models/account-mongo-repository'
+export * from  '../../presentation/controllers/signup/signup';
+export * from  '../../presentation/protocols/controller';
+export * from  "../adapters/email-validator-adapter";
+export * from  "./direct-email-factory";
+
